8b30010235c2951cbb4aaf8d0615760ce25a6aae
encry1024 <register.chihiro@gmail.com>
2018-04-01 16:58:39 +0900

    add notification function

--- webview.js
+++ webview.js
@@ -1,3 +1,13 @@
 'use strict';
 
-module.exports = Franz => {};
+module.exports = Franz => {
+  const getNotification = function() {
+    const el = document.querySelector('.badge_count');
+    let count = 0;
+    if(el.innerHTML) {
+      count = parseInt(el.innerHTML, 10);
+    }
+    Franz.setBadge(count);
+  }
+  Franz.loop(getNotification);
+};
